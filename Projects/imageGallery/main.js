1e23d9081bfb927a8ffc0d7a3cc1e22bf153b0b9
gsv01 <76647046+gsv01@users.noreply.github.com>
2021-05-29 22:30:48 +0100

    Add files via upload

--- Projects/imageGallery/main.js
+++ Projects/imageGallery/main.js
@@ -32,6 +32,32 @@ function goNext(){
 }
 
 
+function changeImage(ximageIndex,id_change){
+    imageIndex = ximageIndex;
+    let projector = document.getElementById('image_projector');
+    projector.setAttribute("style","background-image: url('./gallery/" +images[imageIndex] + "')");
+
+    let img1 = document.getElementById("img1");
+    let img2 = document.getElementById("img2");
+    let img3 = document.getElementById("img3");
+    let img4 = document.getElementById("img4");
+    let img5 = document.getElementById("img5");
+    let img6 = document.getElementById("img6");
+
+
+    img1.setAttribute("style","border: 2px solid white");
+    img2.setAttribute("style","border: 2px solid white");
+    img3.setAttribute("style","border: 2px solid white");
+    img4.setAttribute("style","border: 2px solid white");
+    img5.setAttribute("style","border: 2px solid white");
+    img6.setAttribute("style","border: 2px solid white");
+
+
+
+    let image_icon = document.getElementById(id_change);
+    image_icon.setAttribute("style","border: 2px solid gray");
+}
+
 
 
 
